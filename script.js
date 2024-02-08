83d64cddd5f2eb6c167182ddcc90c7cfed105826
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2024-02-08 16:16:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let tweetContentBox = document.querySelector('textarea');
 let tweetDisplay = document.querySelector(".tweet-display");
 
@@ -26,32 +25,56 @@ postBtn.addEventListener("click", function(e){
            </div>
            <div class="tweet-like-comment-box">
                        
-                     </div>
+           </div>
        `;
        tweetDisplay.appendChild(tweetContentDiv);
        tweetContentBox.value = '';
+
+       // Save tweet to local storage
+       saveTweetToLocalStorage(tweetContent);
     }
 });
 
-let regularHeart = document.querySelectorAll('.heart-icon');
-let likedHeart = document.querySelectorAll('.liked-heart-icon');
-let isLiked = false;
-
-for (let j=0; j<regularHeart.length; j++ ) {
-    regularHeart[j].addEventListener("click",function(e){
-       if (!isLiked) {
-        isLiked = true;
-        regularHeart.classList.add('heart-icon-hide');
-        likedHeart.style.visibility = 'visible';
-       }
-    })
+// Function to save tweet to local storage
+function saveTweetToLocalStorage(tweetContent) {
+    let tweets = [];
+    if (localStorage.getItem('tweets')) {
+        tweets = JSON.parse(localStorage.getItem('tweets'));
+    }
+    tweets.push(tweetContent);
+    localStorage.setItem('tweets', JSON.stringify(tweets));
 }
-for (let j=0; j<likedHeart.length; j++ ) {
-    likedHeart[j].addEventListener("click", function(e){
-        if (isLiked) {
-            isLiked = false;
-            regularHeart.classList.remove('heart-icon-hide');
-            likedHeart.style.visibility = 'visible';
-        }
-    })
+
+// Load tweets from local storage on page load
+window.addEventListener('load', function() {
+    loadTweetsFromLocalStorage();
+});
+
+// Function to load tweets from local storage
+function loadTweetsFromLocalStorage() {
+    let tweets = JSON.parse(localStorage.getItem('tweets'));
+    if (tweets) {
+        tweets.forEach(function(tweetContent) {
+            let tweetContentDiv = document.createElement('div');
+            tweetContentDiv.classList.add('tweet-content-box');
+            tweetContentDiv.innerHTML = `
+                <div class="profile-img">
+                    <img src="https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/031/original/profile_image.png?1706888739" alt="">
+                </div>
+                <div class="tweet-content">
+                    <div class="profile-name-username">
+                        <h1 class="profile-name">Harshit Shah</h1>
+                        <h1 class="profile-username">@harshit.shah</h1>
+                    </div>
+                    <div class="tweet-content-text">
+                        ${tweetContent}
+                    </div>
+                </div>
+                <div class="tweet-like-comment-box">
+                           
+                </div>
+            `;
+            tweetDisplay.appendChild(tweetContentDiv);
+        });
+    }
 }
